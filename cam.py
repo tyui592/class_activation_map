6370dde9b47931eb509c6e33d31a2df3998915af
minseong <tyui592@gmail.com>
2019-11-24 01:04:33 +0900

    major update: add 'network' argument to change backbone network

--- cam.py
+++ cam.py
@@ -1,4 +1,3 @@
-import os
 import json
 import torch
 import argparse
@@ -11,6 +10,9 @@ def build_parser():
 
     parser.add_argument('--gpu-no', type=int,
                     help='cpu: -1, gpu: 0 ~ n ', default=0)
+
+    parser.add_argument('--network', type=str,
+                    help='network to generate class activation map', default='resnet50')
 
     parser.add_argument('--image', type=str,
                     help='path of image', required=True)
@@ -43,7 +45,7 @@ if __name__ == "__main__":
 
     # set device
     device = torch.device('cuda:%d'%args.gpu_no if args.gpu_no >= 0 else 'cpu')
-    network = CAM().to(device)
+    network = CAM(args.network).to(device)
     network.eval()
     image = imload(args.image, args.imsize, args.cropsize).to(device)
 
@@ -57,7 +59,6 @@ if __name__ == "__main__":
 
         for k in range(args.topk):
             print("Predict '%s' with %2.4f probability"%(idx_to_label[cls[k]], prob[k]))
-
             cam_ = cam[k].squeeze().cpu().data.numpy()
             cam_pil = array_to_cam(cam_)
             cam_pil.save(args.save_path+"cam_class__%s_prob__%2.4f.jpg"%(idx_to_label[cls[k]], prob[k]))
